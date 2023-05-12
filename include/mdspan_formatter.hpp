b2b76688b91d7bdd3738f5f927917edf44baa2ab
stripe2933 <stripe2933@gmail.com>
2023-05-12 11:33:33 +0900

    Remove unnecessary namespace qualifier and hardcoded CMake target name.

--- include/mdspan_formatter.hpp
+++ include/mdspan_formatter.hpp
@@ -14,7 +14,7 @@
 #include <mdspan/mdspan.hpp>
 
 template <typename T, typename Extents, typename CharT>
-struct std::formatter<Kokkos::mdspan<T, Extents>, CharT> : std::range_formatter<std::remove_cv_t<T>, CharT>{
+struct std::formatter<Kokkos::mdspan<T, Extents>, CharT> : range_formatter<remove_cv_t<T>, CharT>{
 public:
     template <typename FormatContext>
     constexpr auto format(Kokkos::mdspan<T, Extents> x, FormatContext &ctx) const{
@@ -23,30 +23,30 @@ public:
 
 private:
     template <typename InnerExtents, typename FormatContext>
-    constexpr auto recursive_format(Kokkos::mdspan<T, InnerExtents> x, FormatContext &ctx, std::size_t depth) const{
+    constexpr auto recursive_format(Kokkos::mdspan<T, InnerExtents> x, FormatContext &ctx, size_t depth) const{
         if constexpr (InnerExtents::rank() == 1UZ){
             // Format 1-dimensional span using specified format.
             const auto span = [x]{
-                if constexpr (InnerExtents::static_extent(0) == std::dynamic_extent){
-                    return std::span { x.data_handle(), static_cast<std::size_t>(x.extent(0)) };
+                if constexpr (InnerExtents::static_extent(0) == dynamic_extent){
+                    return std::span { x.data_handle(), static_cast<size_t>(x.extent(0)) };
                 }
                 else{
                     return std::span<T, InnerExtents::static_extent(0)> { x.data_handle(), x.data_handle() + InnerExtents::static_extent(0) };
                 }
             }();
 
-            std::range_formatter<std::remove_cv_t<T>, CharT>::format(span, ctx);
+            range_formatter<remove_cv_t<T>, CharT>::format(span, ctx);
         }
         else{
             // Get extents<T, I2, ..., IN> from extents<T, I1, I2, ..., IN> (skip head template parameter).
-            using subextents = decltype([]<std::size_t I, std::size_t... J>(std::index_sequence<I, J...>){
+            using subextents = decltype([]<size_t I, size_t... J>(index_sequence<I, J...>){
                 return Kokkos::extents<typename InnerExtents::index_type, InnerExtents::static_extent(J)...>{};
-            }(std::make_index_sequence<InnerExtents::rank()>{}));
+            }(make_index_sequence<InnerExtents::rank()>{}));
 
             // Get number of dynamic extents in subextents.
-            constexpr std::size_t num_dynamic_subextents = []<std::size_t... I>(std::index_sequence<I...>){
-                return (static_cast<std::size_t>(subextents::static_extent(I) == std::dynamic_extent) + ...);
-            }(std::make_index_sequence<subextents::rank()>{});
+            constexpr size_t num_dynamic_subextents = []<size_t... I>(index_sequence<I...>){
+                return (static_cast<size_t>(subextents::static_extent(I) == dynamic_extent) + ...);
+            }(make_index_sequence<subextents::rank()>{});
 
             // Get x[0, ...], x[1, ...], ..., x[x.extents(0)-1, ...] view.
             const auto subspans = [=](){
@@ -55,24 +55,24 @@ private:
                 if constexpr (num_dynamic_subextents > 0UZ) {
                     // Since subextents contains dynamic_extent, dynamic extents should be manually passed to
                     // mdspan constructor with fixed-size array type.
-                    std::array<std::size_t, num_dynamic_subextents> dynamic_subextents;
+                    array<size_t, num_dynamic_subextents> dynamic_subextents;
                     for (auto i = 0UZ, j = 0UZ; j < num_dynamic_subextents; ++i) {
-                        if (subextents::static_extent(i) == std::dynamic_extent) {
+                        if (subextents::static_extent(i) == dynamic_extent) {
                             dynamic_subextents[j++] = x.extent(i + 1);
                         }
                     }
 
-                    return std::views::iota(static_cast<Extents::index_type>(0), x.extent(0))
-                           | std::views::transform([=](auto idx) { return Kokkos::mdspan<T, subextents> { x.data_handle() + stride * idx, dynamic_subextents }; });
+                    return views::iota(static_cast<Extents::index_type>(0), x.extent(0))
+                           | views::transform([=](auto idx) { return Kokkos::mdspan<T, subextents> { x.data_handle() + stride * idx, dynamic_subextents }; });
                 }
                 else{
-                    return std::views::iota(static_cast<Extents::index_type>(0), x.extent(0))
-                           | std::views::transform([=](auto idx) { return Kokkos::mdspan<T, subextents> { x.data_handle() + stride * idx }; });
+                    return views::iota(static_cast<Extents::index_type>(0), x.extent(0))
+                           | views::transform([=](auto idx) { return Kokkos::mdspan<T, subextents> { x.data_handle() + stride * idx }; });
                 }
             }();
 
-            std::format_to(ctx.out(), "[");
-            for (auto &&subspan : subspans | std::views::take(subspans.size() - 1UZ)){
+            format_to(ctx.out(), "[");
+            for (auto &&subspan : subspans | views::take(subspans.size() - 1UZ)){
                 recursive_format(subspan, ctx, depth + 1);
 
                 /*
@@ -80,10 +80,10 @@ private:
                  * [[1, 2, 3],       [[1, 2, 3],
                  * [4, 5, 6]]    ->   [4, 5, 6]] (improved readability)
                  */
-                std::format_to(ctx.out(), ",\n{0: ^{1}}", "", depth);
+                format_to(ctx.out(), ",\n{0: ^{1}}", "", depth);
             }
             recursive_format(subspans.back(), ctx, depth + 1);
-            std::format_to(ctx.out(), "]");
+            format_to(ctx.out(), "]");
         }
         return ctx.out();
     }
