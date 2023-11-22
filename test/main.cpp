8461c17f62b83faa8e19cc2d4767e7fdc3fb477c
gomkyung2 <stripe2933@gmail.com>
2023-11-22 21:23:54 +0900

    argc and argv is not required.

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
 #include "tests.hpp"
 
-int main(int argc, char **argv) {
+int main() {
     mdspan_formatter_test::std_formatter::layout_right_test();
     mdspan_formatter_test::std_formatter::layout_left_test();
     mdspan_formatter_test::fmt_formatter::layout_right_test();
